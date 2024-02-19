d1887f8bbcf2820f24a5ea7a3fda2bd108083418
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-02-19 10:17:42 -0500

    I don't know how to fix the warnings

--- src/CAinterfacialresponse.hpp
+++ src/CAinterfacialresponse.hpp
@@ -15,7 +15,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
 
 // Interfacial repsonse function with various functional forms.
 struct InterfacialResponseFunction {
